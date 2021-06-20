b9e7ada73f28a096d86e10d5d06b214b18efcf60
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-06-20 14:15:24 +0300

    fix: push directory one level up

--- users_database/src/modules/removeUser.js
+++
@@ -1,8 +0,0 @@
-import updateTable from "./updateTable"
-
-export default function removeUser(id){
-    let data = JSON.parse(localStorage.getItem('tempdata'))
-    data = data.filter(user => user.id !== id)
-    localStorage.setItem('tempdata', JSON.stringify(data))
-    updateTable(data)
-}
